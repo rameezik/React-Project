ab745f728c19b1edd8cb05f565d5366f7e2ea7bd
Rameez Imran Khan <Rameezimrankhan02@gmail.com>
2026-07-19 06:56:26 +0500

    Made the UI responsive

--- Tasky/src/componenets/home.jsx
+++ Tasky/src/componenets/home.jsx
@@ -1,31 +1,33 @@
-const Note = ()=>{
+const Note = () => {
 
 
 
 
-    return(
+    return (
         <div>
             <div className="bg-[radial-gradient(circle,#f59e0b,#fcd34d,#d97706)] p-2">
                 <h1 className="text-center mb-0 pb-0 text-white text-2xl font-extrabold font-serif">TASKY</h1>
                 <p className="text-center mt-0 pt-0 text-[10px]">Your Daily Task Tracker</p>
             </div>
-        
-        <div className=" bg-[radial-gradient(circle,#2563EB,#00299a)] text-white min-h-screen flex ">
-            
-            <form className=" lg:w-1/2 p-6 lg:border-r-amber-400 lg:border-dashed border-r-2 min-h-screen ">
-              <div className="flex flex-col items-center gap-4">
-                  <h2 className="text-amber-300 font-bold text-[14px] font-serif mb-0 ">PLOT YOUR TASK</h2>
-                <input type="text" placeholder="Enter Task Title" className="border-2 border-amber-300 rounded-b-sm px-3 py-1 w-80" />
-                <textarea placeholder="Enter Task Detail" className="border-2 border-amber-300 rounded-b-sm px-3 py-1 w-80 "></textarea>
-                <button type="submit" className="bg-amber-300 border-2 text-[#0832a5] border-gray-300 rounded-2xl px-2 py-1 active:scale-80">Plot Task</button>
-              </div>
-             </form>
-             <div className="lg:w-1/2">
-             <h2 className="text-amber-300 font-bold text-[14px] font-serif mb-0 p-6 text-center">NOTED TASKS</h2>
-                <img src="note.png" alt="note image" className="h-[250px] w-[170px]" />
-             </div>
-            
-        </div>
+
+            <div className=" bg-[radial-gradient(circle,#2563EB,#00299a)] text-white min-h-screen lg:flex flex-wrap ">
+
+                <form className=" lg:w-1/2  lg:border-r-amber-400 border-dashed lg:border-r-2 lg:min-h-screen p-6">
+                    <div className="flex flex-col items-center gap-4 ">
+                        <h2 className="text-amber-300 font-bold text-[14px] font-serif mb-0  ">PLOT YOUR TASK</h2>
+
+                        <input type="text" placeholder="Enter Task Title" className="border-2 border-amber-300 rounded-b-sm px-3 py-2 w-full bg-white text-gray-800 " />
+
+                        <textarea placeholder="Enter Task Detail" className="border-2 border-amber-300 rounded-b-sm px-3 py-2 w-full h-32 bg-white text-gray-800 "></textarea>
+                        <button type="submit" className="bg-[radial-gradient(circle,#f59e0b,#fcd34d,#d97706)] border-2 text-white font-bold border-white rounded-b-sm px-2 py-1 active:scale-95 w-full">Plot Task</button>
+                    </div>
+                </form>
+                <div className="lg:w-1/2 p-6">
+                    <h2 className="text-amber-300 font-bold text-[14px] font-serif mb-0 text-center">NOTED TASKS</h2>
+                    <span><img src="note.png" alt="note image" className="h-[250px] w-[170px]" /></span>
+                </div>
+
+            </div>
         </div>
     )
 }
