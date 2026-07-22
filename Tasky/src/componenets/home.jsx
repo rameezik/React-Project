75db6c98ad46e63ba5fdfe4a94ff24ea38aefcd1
Rameez Imran Khan <Rameezimrankhan02@gmail.com>
2026-07-23 03:12:23 +0500

    Input Controlling feature added

--- Tasky/src/componenets/home.jsx
+++ Tasky/src/componenets/home.jsx
@@ -1,10 +1,25 @@
+import { useState } from "react";
+
 const Note = () => {
 
+    const [title, setTitle] = useState("")
+    const [details, setDetails] = useState("")
+    const [task, settask] = useState([])
 
+
+    
+    const submitHandler = (e)=>{
+        e.preventDefault();
+        const copyTask = [...task];
+        copyTask.push({title, details});
+        settask(copyTask)
+        setTitle("")
+        setDetails("") 
+    }
 
 
     return (
-        <div>
+        <>
             <div className="bg-[radial-gradient(circle,#f59e0b,#fcd34d,#d97706)] p-2">
                 <h1 className="text-center mb-0 pb-0 text-white text-2xl font-extrabold font-serif">TASKY</h1>
                 <p className="text-center mt-0 pt-0 text-[10px]">Your Daily Task Tracker</p>
@@ -12,23 +27,52 @@ const Note = () => {
 
             <div className=" bg-[radial-gradient(circle,#2563EB,#00299a)] text-white min-h-screen lg:flex flex-wrap ">
 
-                <form className=" lg:w-1/2  lg:border-r-amber-400 border-dashed lg:border-r-2 lg:min-h-screen p-6">
+                <form onSubmit={(e)=>{
+                    submitHandler(e);
+                }} className=" lg:w-1/2  lg:border-r-amber-400 border-dashed lg:border-r-2 lg:min-h-screen p-6"
+                >
                     <div className="flex flex-col items-center gap-4 ">
                         <h2 className="text-amber-300 font-bold text-[14px] font-serif mb-0  ">PLOT YOUR TASK</h2>
 
-                        <input type="text" placeholder="Enter Task Title" className="border-2 border-amber-300 rounded-b-sm px-3 py-2 w-full bg-white text-gray-800 " />
+                        <input onChange = {(e)=>{
+                           const  title = e.target.value
+                            setTitle(title)
+                        }} value={title} type="text" placeholder="Enter Task Title" className="border-2 border-amber-300 rounded-b-sm px-3 py-2 w-full bg-white sm:text-[12px] lg:text-[16px] text-gray-600 font-bold " />
 
-                        <textarea placeholder="Enter Task Detail" className="border-2 border-amber-300 rounded-b-sm px-3 py-2 w-full h-32 bg-white text-gray-800 "></textarea>
+                        <textarea onChange={(e)=>{
+                            const details = e.target.value;
+                            setDetails(details)
+                        }} 
+                        value={details}
+                        placeholder="Enter Task Detail" className="border-2 border-amber-300 rounded-b-sm px-3 py-2 w-full h-32 bg-white lg:text-[16px] text-gray-600 font-semibold "></textarea>
                         <button type="submit" className="bg-[radial-gradient(circle,#f59e0b,#fcd34d,#d97706)] border-2 text-white font-bold border-white rounded-b-sm px-2 py-1 active:scale-95 w-full">Plot Task</button>
                     </div>
                 </form>
-                <div className="lg:w-1/2 p-6">
-                    <h2 className="text-amber-300 font-bold text-[14px] font-serif mb-0 text-center">NOTED TASKS</h2>
-                    <span><img src="note.png" alt="note image" className="h-[250px] w-[170px]" /></span>
+                <div className="lg:w-1/2 p-6 flex justify-between gap-2">
+                {task.map((elem, idx)=>{
+                    console.log(elem)
+
+                    return (
+                        <div key={idx} style={{
+    height: "220px",
+    width: "100px",
+    backgroundImage: "url('/note.png')",
+    backgroundSize: "cover",
+  }}
+                         className=" flex flex-col gap-1 items-start p-0">
+                            <h2 className="">{elem.title}</h2>
+                            <p>{elem.details}</p>
+                        </div>
+                    )
+                }
+
+
+
+                )}
                 </div>
 
             </div>
-        </div>
+        </>
     )
 }
 
