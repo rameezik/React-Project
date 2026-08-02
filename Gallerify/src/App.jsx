137f7c2a8a215562df8d57613ce38203765d278d
Rameez Imran Khan <Rameezimrankhan02@gmail.com>
2026-08-02 07:22:52 +0500

    state mange, transitions added

--- Gallerify/src/App.jsx
+++ Gallerify/src/App.jsx
@@ -6,50 +6,58 @@ import Spinner from "./component/spinner";
 
 
 
-export default function App(){
+export default function App() {
 
   const [data, setData] = useState([]);
+  const [loading, setloading] = useState(true)
 
-  useEffect(()=>{
+  useEffect(() => {
     getData();
-  },[])
+  }, [])
 
-  
-  async function getData(){
-    
+
+  async function getData() {
+
     const response = await fetch("https://picsum.photos/v2/list?page=2&limit=50");
     const data = await response.json()
     setData(data);
-  }
-  let loading = <Spinner />
-  if(data.length>0){
-    loading = data.map((obj, idx)=>{
+    setloading(false)
 
-      
-      return(
-            
-           <>
-              <Card Name={obj.author} Image={obj.url} Url={obj.download_url} key={idx}/>
-          </>
-      )
-    })
-   
+
   }
-   
-  
-  
-  return(
+
+
+
+
+
+
+  return (
     <div className=" h-screen overflow-auto flex flex-col gap-y-8 relative bg-[url('https://images.pexels.com/photos/34393350/pexels-photo-34393350.jpeg')] bg-cover ">
-     <div className="sticky top-0 p-0 z-50">
-      <Navbar />
-     </div>
-   
-    <div className="columns-2 md:columns-3 lg:columns-3 gap-y-4 px-4 ">
-      {loading}
+      <div className="sticky top-0 p-0 z-50">
+        <Navbar />
+      </div>
+      <div
+        className=
+        {`${loading == false
+            ? "columns-2 md:columns-3 lg:columns-3"
+            : "flex justify-center h-screen items-center "
+          } gap-y-4 px-4`}
+      >
+
+        {loading ? <div className="flex flex-col justify-center items-center"><Spinner /> ; <h4 className="font-bold mt-0 p-0 text-gray-400 text-center">Data Fetching...</h4></div> : data.map((obj, idx) => {
+
+
+          return (
+
+            <>
+              <Card Name={obj.author} Image={obj.url} Url={obj.download_url} key={idx} />
+            </>
+          )
+        })}
+      </div>
+
+
     </div>
 
-    
-    </div>
-  
   )
 }
--- Gallerify/src/component/card.jsx
+++ Gallerify/src/component/card.jsx
@@ -1,5 +1,5 @@
-import Spinner from "./spinner"
 import { useState } from "react"
+import { BeatLoader } from "react-spinners"
 
 
 
@@ -16,30 +16,35 @@ export default function Card({ Image, Url, Name }, key) {
 
 
       {load && (
-        <div className="absolute inset-0 flex justify-center items-center z-10">
-          <Spinner />
+        <div className="absolute inset-0 flex justify-center items-center z-10 flex-col bg-[rgb(146,146,146,0.6)] rounded-xl">
+          <BeatLoader size={4}  color="#fff"/>
+          <h6 className=" text-gray-400 text-[12px] text-center">Image Loading..</h6>
         </div>
       )}
 
-      <a className="cursor-pointer h-full w-full text-white visited:text-red-500 " 
+
+         <a className="cursor-pointer h-full w-full text-white visited:text-red-500 " 
       href={Image} target="_blank">
         <img 
 
-         className={`${load ? "opacity-0" : "opacity-100"} object-cover rounded-xl border-2 border-[#062223]`}
+         className={`${load ? "opacity-0" : "opacity-100"} transition-opacity duration-500  object-cover rounded-xl border-2 border-[#062223]`}
 
           src={Url} alt="image"
 
           onLoad={() => 
-            { console.log("imageloaded"); setloading(false) }
+            { setloading(false) }
           }
           
           onError={() => {
             setloading(false)
           }} />
 
-        <h2 className="ml-1   font-semibold font-mono ">{Name}</h2>
+         {load? <h2> </h2>:
+         <h2 className=" ml-1 font-semibold font-mono " >{Name}</h2>}
 
-      </a>
+        </a>
+     
+     
 
     </div>
   )
